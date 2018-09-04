0a59b54a376c8d6071c74febcf26f975c2d960da
Cortney Gardner <cortneygardner@PC-LM31571.overstock.com>
2018-09-04 10:46:50 -0600

    fixed verbage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :notes
+  resources :note
 
-  root 'notes#index'
+  root 'note#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
